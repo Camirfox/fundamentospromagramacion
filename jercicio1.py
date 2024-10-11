bd31b0b7760b7dd4f3531429e8b5d56654dbdbad
Amir <camirfox.12@gmail,com>
2024-10-11 12:43:13 -0600

    01 jercicio1.py

---
+++ jercicio1.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 1:
+Escribir un programa que pregunte al usuario su nombre
+e imprimmir un saludo
+Entradas: 
+nombre: string
+salida:
+saludo: string
+"""
+nombre = input("Escribe tu nombre:")
+saludo = "Hola " + nombre + "!"
+print(saludo)
