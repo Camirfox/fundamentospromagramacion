36bd641a32c8df5f83d63d3dfcfd019090e09378
Amir <camirfox.12@gmail,com>
2024-10-23 10:22:01 -0600

    04 ejercicio 10

---
+++ ejercici10.py
@@ -0,0 +1,20 @@
+"""
+entrada:
+cal1,cal2,cal3,examenfinal,trabajofinal
+salidas:
+calificacionfinal
+"""
+cal1=input("ingresa las tres calificaciones:" )
+cal1=int(cal1)
+
+cal2=input("calificacion2:" )
+cal2=int(cal2)
+
+cal3=input("calificacion3:" )
+cal3=int(cal3)
+
+examfinal=input("ingresa la calificacion del examen final:" ) 
+examfinal=int(examfinal)
+
+trabajofinal=input("ingresa la calficacion del trabajofinal:" )
+trabajofinal=int(trabajofinal)
